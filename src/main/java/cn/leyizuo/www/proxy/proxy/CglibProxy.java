8e25f630e9723c317dc2e81e260de37e09377d5a
qiusheng <592389239@qq.com>
2019-06-19 09:23:59 +0800

    怎么特么还有未提交的

--- src/main/java/cn/leyizuo/www/proxy/proxy/CglibProxy.java
+++ src/main/java/cn/leyizuo/www/proxy/proxy/CglibProxy.java
@@ -1,4 +1,10 @@
-package cn.leyizuo.www.proxy.Proxy;
+package cn.leyizuo.www.proxy.proxy;
+
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
 
 /**
  * @Author： qiusheng
@@ -6,5 +12,34 @@ package cn.leyizuo.www.proxy.Proxy;
  * @DESCRIPTION：
  * @EMAIL： atticusma@hotmail.com
  */
-public class CglibProxy {
+public class CglibProxy implements MethodInterceptor {
+    private Object target;
+    public CglibProxy(Object target){
+        this.target=target;
+    }
+
+    public Object getProxyInstance(){
+        //工具类，执行创建子类
+        Enhancer en = new Enhancer();
+        en.setSuperclass(target.getClass());
+        en.setCallback(this);
+        return en.create();
+    }
+
+    /**
+     * 这么多的参数都是干嘛的
+     * @param o
+     * @param method
+     * @param objects
+     * @param methodProxy
+     * @return
+     * @throws Throwable
+     */
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        System.out.println("Cglib执行了拦截");
+        Object returnValue = method.invoke(target,objects);
+        System.out.println("Cglib拦截结束");
+        return returnValue;
+    }
 }
